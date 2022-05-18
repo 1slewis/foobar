09a4cc45d59447d3bb042d582f0dcc08bba00b4d
1slewis <77329743+1slewis@users.noreply.github.com>
2022-05-19 00:57:54 +0100

    Update Solution.java

--- BinaryTreeTransversal (Level 2b)/Solution.java
+++ BinaryTreeTransversal (Level 2b)/Solution.java
@@ -1,1 +1,36 @@
+import java.util.Arrays;
 
+public class Solution {
+    public static int recursiveMethod(int h, int qi) {
+        int startNum = 0;
+        for (int i = 0; i < h; i++) {
+            startNum = (2 * startNum + 1);
+            if (qi == startNum && h > (i + 1)) {
+                return (2 * startNum) + 1;
+            }
+            if (qi == 1 && h > (i + 1)) {
+                return 3;
+            }
+            if (qi == (startNum - 1)) {
+                return startNum;
+            }
+            if (qi < startNum) {
+                int add = ((startNum - 1) / 2);
+                return (add + Solution.recursiveMethod(i, qi - add));
+            }
+            return -1;
+        }
+    }
+    public static int[] solution(int h, int[] q) {
+        // Your code here
+        int[] output = new int[q.length];
+        for (int i = 0; i < q.length; i++) {
+            output[i] = Solution.recursiveMethod(h, q[i]);
+        }
+        return output;
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(Solution.solution(5, new int[]{19, 14, 28}))); // [21,15,29]
+        System.out.println(Arrays.toString(Solution.solution(3, new int[]{7, 3, 5, 1}))); // [-1,7,6,3]
+    }
+}
