545ba219088496c2ac3024b47cfd54cce0c8f097
1slewis <77329743+1slewis@users.noreply.github.com>
2022-05-23 11:52:56 +0100

    Update Solution.java

--- ShortestRouteToTake (Level 3a)/Solution.java
+++ ShortestRouteToTake (Level 3a)/Solution.java
@@ -1,92 +1,12 @@
-import java.util.*;
+import java.util.ArrayList;
 
 public class Solution {
-    public static boolean isXYinHistory(int x, int y, ArrayList<HashMap> historyArrayList) {
-        for (int i = 0; i < historyArrayList.size(); i++) {
-            HashMap<String, Integer> historyIHashMap = historyArrayList.get(i);
-            if (historyIHashMap.get("x") == x && historyIHashMap.get("y") == y) {
-                return true;
-            }
-        }
-        return false;
-    }
-    public static ArrayList recursiveMethodRoutes(int[][] map, int x, int y, ArrayList<HashMap> historyArrayList, Boolean hasRemoved) {
-        ArrayList outputArrayList = new ArrayList();
-        HashMap<String, Integer> historyXYHashMap = new HashMap<String, Integer>();
-        historyXYHashMap.put("x", x);
-        historyXYHashMap.put("y", y);
-        historyArrayList.add(historyXYHashMap);
-        if (x == map[0].length - 1 && y == map.length - 1) {
-            outputArrayList.add(Integer.valueOf(historyArrayList.size()));
-            return outputArrayList;
-        }
-        if (y > 0) {
-            int newY = y - 1;
-            int u = map[newY][x];
-            if (!(isXYinHistory(x, newY, historyArrayList))) {
-                if (u == 0) {
-                    outputArrayList.add(Solution.recursiveMethodRoutes(map, x, newY, new ArrayList<HashMap>(historyArrayList), hasRemoved));
-                } else if (!hasRemoved) {
-                    outputArrayList.add(Solution.recursiveMethodRoutes(map, x, newY, new ArrayList<HashMap>(historyArrayList), true));
-                }
-            }
-        }
-        if (y < map.length - 1) {
-            int newY = y + 1;
-            int d = map[newY][x];
-            if (!(isXYinHistory(x, newY, historyArrayList))) {
-                if (d == 0) {
-                    outputArrayList.add(Solution.recursiveMethodRoutes(map, x, newY, new ArrayList<HashMap>(historyArrayList), hasRemoved));
-                } else if (!hasRemoved) {
-                    outputArrayList.add(Solution.recursiveMethodRoutes(map, x, newY, new ArrayList<HashMap>(historyArrayList), true));
-                }
-            }
-        }
-        if (x > 0) {
-            int newX = x - 1;
-            int l = map[y][newX];
-            if (!(isXYinHistory(newX, y, historyArrayList))) {
-                if (l == 0) {
-                    outputArrayList.add(Solution.recursiveMethodRoutes(map, newX, y, new ArrayList<HashMap>(historyArrayList), hasRemoved));
-                } else if (!hasRemoved) {
-                    outputArrayList.add(Solution.recursiveMethodRoutes(map, newX, y, new ArrayList<HashMap>(historyArrayList), true));
-                }
-            }
-        }
-        if (x < map[0].length - 1) {
-            int newX = x + 1;
-            int r = map[y][newX];
-            if (!(isXYinHistory(newX, y, historyArrayList))) {
-                if (r == 0) {
-                    outputArrayList.add(Solution.recursiveMethodRoutes(map, newX, y, new ArrayList<HashMap>(historyArrayList), hasRemoved));
-                } else if (!hasRemoved) {
-                    outputArrayList.add(Solution.recursiveMethodRoutes(map, newX, y, new ArrayList<HashMap>(historyArrayList), true));
-                }
-            }
-        }
-        if (outputArrayList.size() == 0) {
-            outputArrayList.add(Integer.MAX_VALUE);
-            return outputArrayList;
-        }
-        return outputArrayList;
-    }
-    public static int recursiveMethodShortest(ArrayList inputArrayList, int shortest) {
-        for (int i = 0; i < inputArrayList.size(); i++) {
-            //System.out.println(inputArrayList.get(i).getClass());
-            if (inputArrayList.get(i).getClass() == Integer.valueOf(1).getClass() && ((int)inputArrayList.get(i)) < shortest) {
-                shortest = (int)inputArrayList.get(i); 
-            }
-            if (inputArrayList.get(i).getClass() == ArrayList.class) {
-                shortest = recursiveMethodShortest((ArrayList)inputArrayList.get(i), shortest);
-            }
-        }
-        return shortest;
-    }
     public static int solution(int[][] map) {
         // Your code here
-        ArrayList outputArrayList = recursiveMethodRoutes(map, 0, 0, new ArrayList<HashMap>(), false);
-        int shortest = recursiveMethodShortest(outputArrayList, Integer.MAX_VALUE);
-        return shortest;
+        Solutions solution1 = new Solutions(map);
+        solution1.calc();
+        int output = solution1.ret();
+        return output;
     }
     public static void main(String[] args) {
         System.out.println(Solution.solution(new int[][]{new int[]{0, 1, 1, 0}, new int[]{0, 0, 0, 1}, new int[]{1, 1, 0, 0}, new int[]{1, 1, 1, 0}}));
@@ -94,4 +14,102 @@ public class Solution {
         System.out.println(Solution.solution(new int[][]{new int[]{0, 0, 0, 0, 0, 0}, new int[]{1, 1, 1, 1, 1, 0}, new int[]{0, 0, 0, 0, 0, 0}, new int[]{0, 1, 1, 1, 1, 1}, new int[]{0, 1, 1, 1, 1, 1}, new int[]{0, 0, 0, 0, 0, 0}}));
         //11
     }
+}
+
+
+class Pnt {
+    final int xPnt;
+    final int yPnt;
+    public Pnt(int x, int y) {
+        xPnt = x;
+        yPnt = y;
+    }
+    public int getX() {
+        return xPnt;
+    }
+    public int getY() {
+        return yPnt;
+    }
+}
+class Solutions {
+    final int[][] map;
+    ArrayList<Integer> outputArrayList;
+    public Solutions(int[][] map) {
+        this.outputArrayList = new ArrayList<Integer>();
+        this.map = map;
+        this.outputArrayList = new ArrayList<Integer>();
+    }
+    public void calc() {
+        recursiveMethodRoutes(0, 0, new ArrayList<Pnt>(), false);
+    }
+    public int ret() {
+        int shortest = Integer.MAX_VALUE;
+        for (int i = 0; i < outputArrayList.size(); i++) {
+            if ((int)outputArrayList.get(i) < shortest) {
+                shortest = (int)outputArrayList.get(i);
+            }
+        }
+        return shortest;
+    }
+    public boolean isXYinHistory(int x, int y, ArrayList<Pnt> historyArrayList) {
+        for (int i = 0; i < historyArrayList.size(); i++) {
+            Pnt historyIPnt = historyArrayList.get(i);
+            if (historyIPnt.getX() == x && historyIPnt.getY() == y) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public void recursiveMethodRoutes(int x, int y, ArrayList<Pnt> historyArrayList, Boolean hasRemoved) {
+        Pnt historyXYPnt = new Pnt(x, y);
+        historyArrayList.add(historyXYPnt);
+        if (x == this.map[0].length - 1 && y == this.map.length - 1) {
+            outputArrayList.add(Integer.valueOf(historyArrayList.size()));
+        }
+        if (y > 0) {
+            int newY = y - 1;
+            int u = map[newY][x];
+            if (!(this.isXYinHistory(x, newY, historyArrayList))) {
+                if (u == 0) {
+                    recursiveMethodRoutes(x, newY, new ArrayList<Pnt>(historyArrayList), hasRemoved);
+                } else if (!hasRemoved) {
+                    recursiveMethodRoutes(x, newY, new ArrayList<Pnt>(historyArrayList), true);
+                }
+            }
+        }
+        if (y < map.length - 1) {
+            int newY = y + 1;
+            int d = map[newY][x];
+            if (!(this.isXYinHistory(x, newY, historyArrayList))) {
+                if (d == 0) {
+                    recursiveMethodRoutes(x, newY, new ArrayList<Pnt>(historyArrayList), hasRemoved);
+                } else if (!hasRemoved) {
+                    recursiveMethodRoutes(x, newY, new ArrayList<Pnt>(historyArrayList), true);
+                }
+            }
+        }
+        if (x > 0) {
+            int newX = x - 1;
+            int l = map[y][newX];
+            if (!(this.isXYinHistory(newX, y, historyArrayList))) {
+                if (l == 0) {
+                    recursiveMethodRoutes(newX, y, new ArrayList<Pnt>(historyArrayList), hasRemoved);
+                } else if (!hasRemoved) {
+                    recursiveMethodRoutes(newX, y, new ArrayList<Pnt>(historyArrayList), true);
+                }
+            }
+        }
+        if (x < map[0].length - 1) {
+            int newX = x + 1;
+            int r = map[y][newX];
+            if (!(this.isXYinHistory(newX, y, historyArrayList))) {
+                if (r == 0) {
+                    recursiveMethodRoutes(newX, y, new ArrayList<Pnt>(historyArrayList), hasRemoved);
+                } else if (!hasRemoved) {
+                    recursiveMethodRoutes(newX, y, new ArrayList<Pnt>(historyArrayList), true);
+                }
+            }
+        }
+    }
+    
 }
