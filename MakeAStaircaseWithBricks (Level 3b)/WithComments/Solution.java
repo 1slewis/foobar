7176989c7f86d2ad4e266366b629f06e484687f7
1slewis <77329743+1slewis@users.noreply.github.com>
2022-06-01 13:25:36 +0100

    Update Solution.java

--- MakeAStaircaseWithBricks (Level 3b)/WithComments/Solution.java
+++ MakeAStaircaseWithBricks (Level 3b)/WithComments/Solution.java
@@ -1,7 +1,7 @@
 public class Solution {
     public static void addArrays(int[] arrayA, int[] arrayB) {
         // This function just gets the contents of two arrays and adds them element by element arithmetically
-        // This function mutates arrayA as I am using loops not recursion 
+        // This function mutates arrayA as I am using loops not recursion (However arrayA isnt a public property)
         int length = arrayA.length;
         int[] result = new int[length];
         for (var a = 0; a < length; a++) {
@@ -12,9 +12,9 @@ public class Solution {
         // Main logic for multiplying 1(1+x^1)(1+x^2)...
         // Answer is obtained by finding the coefficent of the x^n term which is how many ways to get x^n and x^a * x^b = x^a+b
         // So the answer a in ax^n is addition of unique numbers 
-        // initiate an array just bigger than n
+        // Initiate an array just bigger than n
         int[] res = new int[n + 1];
-        // first coefficient x^0 == 1 as 1(1+x)(1+x^2) there is an original 1 outside the bracket
+        // First coefficient x^0 == 1 as 1(1+x)(1+x^2) there is an original 1 outside the bracket
         res[0] = 1;
         for (var i = 1; i < n; i++) {
             // Initiate a new Array to contain the x^i * by the already calculated sum
